e6d23990764471df98ef56c41a5ba5b4237a39e0
Mayank Dixit <mayank.dixit_cs22@gla.ac.in>
2025-07-01 08:26:41 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import navbar from './components/navbar/navbar'
+const App = () => {
+  return (
+    <div classname='app'>
+    <navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './navbar.css'
+import { assets } from '../../assets/assets'
+
+const navbar = () => {
+  return (
+    <div className='navbar'>
+<img src={assets.logo} alt="" />
+    </div>
+  )
+}
+
+export default navbar
